0abb30a5fea31ac2a1a73d84426a548b0628fdd2
KimChol <47628393+uAxCXN8Rla@users.noreply.github.com>
2019-07-21 12:31:21 +0800

    V0.4 注解的处理器映射器和适配器

--- src/cn/hp/ssm/controller/ItemController2.java
+++ src/cn/hp/ssm/controller/ItemController2.java
@@ -3,18 +3,15 @@ package cn.hp.ssm.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-
 import cn.hp.ssm.po.Items;
 
-public class ItemController implements Controller {
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request , HttpServletResponse response) throws Exception {
+@org.springframework.stereotype.Controller
+public class ItemController2 {
+	
+	@RequestMapping("/queryItems")
+	public ModelAndView querytItems() throws Exception {
 		
 			//调用service查找 数据库，查询商品列表，这里使用静态数据模拟
 		
