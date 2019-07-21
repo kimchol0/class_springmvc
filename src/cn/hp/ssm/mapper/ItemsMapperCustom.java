522806d7d2f2b1dffcbcb44472b073df235b7c3c
KimChol <47628393+uAxCXN8Rla@users.noreply.github.com>
2019-07-21 12:48:15 +0800

    V0.5 Dao配置文件，逆向工程(MybatisGenerator)生成

---
+++ src/cn/hp/ssm/mapper/ItemsMapperCustom.java
@@ -0,0 +1,11 @@
+package cn.hp.ssm.mapper;
+
+import java.util.List;
+
+import cn.hp.ssm.po.ItemsCustom;
+import cn.hp.ssm.po.ItemsQueryVo;
+
+public interface ItemsMapperCustom {
+	//商品查询列表
+		public List<ItemsCustom> findItemsList(ItemsQueryVo itemsQueryVo)throws Exception;
+}
